2c17adbdad45aa362f89fd35e6b27071fca7b592
Max <maximz2009@gmail.com>
2024-12-12 16:35:09 +0300

    initial

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-	  adapter: adapter({
-		runtime: 'edge'
-	  })
-	}
-  };
-  
-  export default config;
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
